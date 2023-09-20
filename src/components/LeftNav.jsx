35ba5f750bff4b93e84fda52a0b73d13ad17b4ba
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2023-09-20 15:17:47 +0530

    nav

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -55,7 +55,7 @@ const LeftNav = () => {
                 })}
                 <hr className="my-5 border-white/[0.2]" />
                 <div className="text-white/[0.5] text-[12px]">
-                    Clone by: JS Dev Hindi
+                    Aman Kushwaha
                 </div>
             </div>
         </div>
